32de59f6b74568c9da2ec75664071bb071ca45e3
DuncanChristian <45184911+DuncanChristian@users.noreply.github.com>
2019-04-23 08:10:22 -0700

    Add files via upload

--- js/example.js
+++ js/example.js
@@ -1,31 +1,50 @@
-// JavaScript Document
-var greeting = 'Howdy ';
-var names = 'Molly';
-var message = ', please check your order:';
+JavaScript Document
 
-var welcome = greeting + names + message;
 
-var sign = 'Montague House';
-var tiles = sign.length;
-var subTotal = tiles * 5;
-var shipping = 7;
-var grandTotal = subTotal + shipping;
-
-var el = document.getElementById('greeting');
-
-el.textContent = welcome;
-
-var elSign = document.getElementById('userSign');
-elSign.textContent = sign;
-
-var elTiles = document.getElementById('tiles');
-elTiles.textContent = tiles;
-
-var elSubTotal = document.getElementById('subTotal');
-elSubTotal.textContent = '$' + subTotal;
-
-var elShipping = document.getElementById('shipping');
-elShipping.textContent = '$' + shipping;
-
-var elGrandTotal = document.getElementById('grandTotal');
-elGrandTotal.textContent = '$' + grandTotal;
+(function() {
+	var book = {
+		name: 'Sirens',
+		price: 15,
+		discount: 25,
+		offerPrice: function() {
+			var offerRate = this.price * ((100 - this.discount)/100);
+			return offerRate;
+		}
+	};
+	
+	var bookName, bookPrice, specialPrice;
+	
+	bookName = document.getElementById('bookName');
+	bookPrice = document.getElementById('bookPrice');
+	specialPrice = document.getElementById('specialPrice');
+	
+	bookName.textContent = book.name;
+	bookPrice.textContent = '$' + book.price.toFixed(2);
+	specialPrice.textContent = '$' + book.offerPrice;
+	
+	var expiryMsg;
+	var today;
+	var elEnds;
+	
+	function offerExpires(today) {
+		var weekFromToday, day, date, month, year, dayNames, monthNames;
+		
+		weekFromToday = new Date(today.getTime() + 7 * 24 * 60 * 60 * 1000);
+		
+		dayNames = ['Sunday', 'Monday', 'Tuesday','Wednesday','Thursday','Friday','Saturday'];
+		monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+		
+		day = dayNames[weekFromToday.getDay()];
+		date = weekFromToday.getDate();
+		month = monthNames[weekFromToday.getmonth()];
+		year = weekFromToday.getFullYear();
+		
+		expiryMsg = 'Offer expires next ';
+		expiryMsg += day + ' <br />(' + date + ' ' + month + ' ' + year + ')';
+		return expiryMsg;
+	}
+	
+	today = new Date();
+	elEnds = document.getElementById('offerEnds');
+	elEnds.innerHTML = offerExpires(today);
+}());
