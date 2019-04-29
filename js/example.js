8fbacdb8e9b75adc393ee829fdb09066aa0bfde1
DuncanChristian <45184911+DuncanChristian@users.noreply.github.com>
2019-04-29 08:50:11 -0700

    Add files via upload

--- js/example.js
+++ js/example.js
@@ -1,50 +1,22 @@
-JavaScript Document
+var table = 3;
+var operator = 'addition';
+var i = 1;
+var msg = '';
 
+if (operator === 'addition') {
+  // Do addition
+  while (i < 11) {
+    msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
+    i++;
+  }
+} else {
+  // Do multiplication
+  while (i < 11) {
+    msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
+    i++;
+  }
+}
 
-(function() {
-	var book = {
-		name: 'Sirens',
-		price: 15,
-		discount: 25,
-		offerPrice: function() {
-			var offerRate = this.price * ((100 - this.discount)/100);
-			return offerRate;
-		}
-	};
-	
-	var bookName, bookPrice, specialPrice;
-	
-	bookName = document.getElementById('bookName');
-	bookPrice = document.getElementById('bookPrice');
-	specialPrice = document.getElementById('specialPrice');
-	
-	bookName.textContent = book.name;
-	bookPrice.textContent = '$' + book.price.toFixed(2);
-	specialPrice.textContent = '$' + book.offerPrice;
-	
-	var expiryMsg;
-	var today;
-	var elEnds;
-	
-	function offerExpires(today) {
-		var weekFromToday, day, date, month, year, dayNames, monthNames;
-		
-		weekFromToday = new Date(today.getTime() + 7 * 24 * 60 * 60 * 1000);
-		
-		dayNames = ['Sunday', 'Monday', 'Tuesday','Wednesday','Thursday','Friday','Saturday'];
-		monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-		
-		day = dayNames[weekFromToday.getDay()];
-		date = weekFromToday.getDate();
-		month = monthNames[weekFromToday.getmonth()];
-		year = weekFromToday.getFullYear();
-		
-		expiryMsg = 'Offer expires next ';
-		expiryMsg += day + ' <br />(' + date + ' ' + month + ' ' + year + ')';
-		return expiryMsg;
-	}
-	
-	today = new Date();
-	elEnds = document.getElementById('offerEnds');
-	elEnds.innerHTML = offerExpires(today);
-}());
+// Write the message into the page
+var el = document.getElementById('blackboard');
+el.innerHTML = msg;
