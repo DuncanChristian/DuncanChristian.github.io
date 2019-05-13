d4e1cf4fce9136bc003e3c5b3602b3cd2a334abf
DuncanChristian <45184911+DuncanChristian@users.noreply.github.com>
2019-05-13 08:56:32 -0700

    Add files via upload

--- js/example.js
+++ js/example.js
@@ -1,26 +1,54 @@
-//Javascript Document
+$(function() {
 
-var list = document.getElementsByTagName('ul')[0];
+  var $list, $newItemForm, $newItemButton;
+  var item = '';
+  $list = $('ul');
+  $newItemForm = $('#newItemForm');
+  $newItemButton = $('#newItemButton');
 
-var newItemLast = document.createElement('li');
-var newTextLast = document.createTextNode('cream');
-newItemLast.appendChild(newTextLast);
-list.appendChild(newItemLast);
+  $('li').hide().each(function(index) {
+    $(this).delay(450 * index).fadeIn(1600);
+  });
 
-var newItemFirst = document.createElement('li');
-var newTextFirst = document.createTextNode('Mother Night');
-newItemFirst.appendChild(newTextFirst);
-list.insertBefore(newItemFirst, list.firstChild);
+  function updateCount() {
+    var items = $('li[class!=complete]').length;
+    $('#counter').text(items);
+  }
+  updateCount();
 
-var listItems = document.querySelectorAll('li');
+  $newItemButton.show();
+  $newItemForm.hide();
+  $('#showForm').on('click', function() {
+    $newItemButton.hide();
+    $newItemForm.show();
+  });
 
-var i;
-for (i = 0; i < listItems.length; i++) {
-  listItems[i].className = 'cool';
-}
+  $newItemForm.on('submit', function(e) {
+    e.preventDefault();
+    var text = $('input:text').val();
+    $list.append('<li>' + text + '</li>');
+    $('input:text').val('');
+    updateCount();
+  });
 
-var heading = document.querySelector('h2');
-var headingText = heading.firstChild.nodeValue;
-var totalItems = listItems.length;
-var newHeading =  headingText + '<span>' + totalItems + '</span>';
-heading.innerHTML = newHeading;
+  $list.on('click', 'li', function() {
+    var $this = $(this);
+    var complete = $this.hasClass('complete');
+
+    if (complete === true) {
+      $this.animate({
+        opacity: 0.0,
+        paddingLeft: '+=180'
+      }, 500, 'swing', function() {
+        $this.remove();
+      });
+    } else {
+      item = $this.text();
+      $this.remove();
+      $list
+        .append('<li class=\"complete\">' + item + '</li>')
+        .hide().fadeIn(300);
+      updateCount();
+    }
+  });
+});
